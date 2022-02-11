bc8b62cfa3f98cc5f94c1f8cb2a26db168a967ad
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-02-11 17:25:06 +0600

    Update BubbleSort.py

--- PYTHON/Sorting/BubbleSort.py
+++ PYTHON/Sorting/BubbleSort.py
@@ -1,27 +1,29 @@
 # Python program for implementation of Bubble Sort
 
-def bubbleSort(arr):
-	n = len(arr)
+# Logic
+def BubbleSort(list):
+    tempList = list  # Store a snapshot of the list in a temporary variable
 
-	# Traverse through all array elements
-	for i in range(n-1):
-	# range(n) also work but outer loop will
-	# repeat one time more than needed.
+    # Iterate through the list, where "i" is the index of the list
+    for i in range(len(tempList)):
+        j = 1  # Set the value of "j" to 1, to compare the second element with the first element
 
-		# Last i elements are already in place
-		for j in range(0, n-i-1):
+        # Bruteforcing the list, where "j" is the current index of the list.
+        # While "j" is less than the length of the list minus 1, compare the current element with the previous element
+        while j < len(tempList) - i:
+            # Compare the current element with the previous element
+            if tempList[j] < tempList[j - 1]:
+                tempList[j], tempList[j - 1] = tempList[j -
+                                                        1], tempList[j]  # Swap the elements if the current element is less than the previous element
 
-			# traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
-			if arr[j] > arr[j + 1] :
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            j += 1  # Increment the value of "j" by 1, to proceed to the next iteration
 
-# Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
+    return tempList  # Return the sorted list
 
-bubbleSort(arr)
 
-print ("Sorted array is:")
-for i in range(len(arr)):
-	print ("% d" % arr[i],end=" ")
+# Test
+list = [64, 34, 25, 12, 22, 11, 90]  # Create a demo list
+
+sortedList = BubbleSort(list)  # Sort the list, with our function "BubbleSort"
+
+print("Sorted array is:", sortedList)  # Print the sorted list!
